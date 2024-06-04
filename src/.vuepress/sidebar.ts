1be4b2b1b987ee0d976909dacbf271601b16c507
haley <huanghongweimax@163.com>
2024-06-04 14:19:13 +0800

    添加了几篇游戏攻略

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -10,12 +10,12 @@ export default sidebar({
       link: "demo/",
       children: "structure",
     },*/
-    /*{
+    {
       text: "文章",
       icon: "book",
       prefix: "posts/",
       children: "structure",
-    },*/
+    },
     {
       text: "工具",
       icon: "book",
